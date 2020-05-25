3dd5216cbc3ee7c9060732ffb93500e4489059ab
Greg Depoire--Ferrer <greg.depoire@gmail.com>
2020-05-25 13:21:24 +0200

    Show '(empty)' instead of nothing when nothing is typed

--- a22abb2/A22abb2/CalculationViewModel.cs
+++ a22abb2/A22abb2/CalculationViewModel.cs
@@ -15,6 +15,8 @@ namespace A22abb2
                 {
                     this.expression = value;
                     this.OnPropertyChanged(nameof(Expression));
+                    this.OnPropertyChanged(nameof(ExpressionText));
+                    this.OnPropertyChanged(nameof(IsEmpty));
 
                     // TODO: do not call this on the UI thread
                     var newResult = A22abb2.Eval(this.expression.Trim());
@@ -28,15 +30,30 @@ namespace A22abb2
             }
         }
 
+        public string ExpressionText
+        {
+            get => this.IsEmpty ? "(empty)" : this.expression;
+        }
+
+        public bool IsEmpty
+        {
+            get => string.IsNullOrWhiteSpace(this.expression);
+        }
+
         public double Result { get; private set; } = double.NaN;
+
+        public bool IsValid
+        {
+            get => !double.IsNaN(this.Result);
+        }
 
         public string ResultText
         {
             get
             {
-                if (string.IsNullOrWhiteSpace(this.expression))
+                if (this.IsEmpty)
                 {
-                    return "";
+                    return "(empty)";
                 }
                 else if (double.IsNaN(this.Result))
                 {
